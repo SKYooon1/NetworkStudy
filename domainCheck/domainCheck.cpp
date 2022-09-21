1c01210f8f1179084ae7032b6638b2bf184bc942
SKYooon <ucs0191@naver.com>
2022-09-21 23:29:05 +0900

    과제 3.2 - 도메인 확인 프로그램

--- domainCheck/domainCheck.cpp
+++ domainCheck/domainCheck.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
-
 #include "../Common.h"
 
-#define TESTNAME "www.google.com"
-
 // 도메인 이름 -> IPv4 주소
-bool GetIPAddr(const char* name, struct in_addr* addr)
+void domainCheck(const char* name)
 {
 	struct hostent* ptr = gethostbyname(name);
 	if (ptr == NULL) {
 		err_display("gethostbyname()");
-		return false;
+		return;
 	}
 	if (ptr->h_addrtype != AF_INET)
-		return false;
-	memcpy(addr, ptr->h_addr, ptr->h_length);
+		return;
 
-	return true;
+	struct in_addr addr {};
+	char str[INET_ADDRSTRLEN];
+
+	std::cout << "호스트 이름: " << ptr->h_name << std::endl;
+
+	if (ptr->h_aliases[0] == nullptr)
+		std::cout << "별명이 없습니다." << std::endl;
+	else for (int i{}; ptr->h_aliases[i] != nullptr; ++i)
+		std::cout << i + 1 << "번째 별명: " << ptr->h_aliases[i] << std::endl;
+
+	for(int i{}; ptr->h_addr_list[i] != nullptr; ++i)
+	{
+		memcpy(&addr, ptr->h_addr_list[i], ptr->h_length);
+		inet_ntop(AF_INET, &addr, str, sizeof(str));
+		std::cout << i + 1 << "번째 IP 주소: " << str << std::endl;
+	}
+
+	std::cout << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -26,16 +39,15 @@ int main(int argc, char* argv[])
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
 
+	std::string str;
 
-	// 도메인 이름 -> IP 주소
-	struct in_addr addr;
-	if (GetIPAddr(TESTNAME, &addr)) {
-		// 성공이면 결과 출력
-		char str[INET_ADDRSTRLEN];
-		inet_ntop(AF_INET, &addr, str, sizeof(str));
-		std::cout << "IP 주소 변환 후: " << str;
+	while (true)
+	{
+		std::cout << "도메인을 입력해주세요: ";
+		std::cin >> str;
 
-		
+		// 도메인 이름 -> IP 주소
+		struct in_addr addr;
+		domainCheck(str.c_str());
 	}
-
 }
