32e2f08c1ec1507f56c92c7ae6fa29aa9dbac156
SKYooon <ucs0191@naver.com>
2022-09-22 12:31:28 +0900

    과제 3.2 - 명령행 인수를 입력받는 프로그램으로 수정

--- domainCheck/domainCheck.cpp
+++ domainCheck/domainCheck.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "../Common.h"
 
-// 도메인 이름 -> IPv4 주소
+void domainCheck(const char* name);
+
+int main(int argc, char* argv[])
+{
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// 도메인 이름 -> IP 주소
+	struct in_addr addr;
+	domainCheck(argv[1]);
+}
+
 void domainCheck(const char* name)
 {
 	struct hostent* ptr = gethostbyname(name);
@@ -22,7 +35,7 @@ void domainCheck(const char* name)
 	else for (int i{}; ptr->h_aliases[i] != nullptr; ++i)
 		std::cout << i + 1 << "번째 별명: " << ptr->h_aliases[i] << std::endl;
 
-	for(int i{}; ptr->h_addr_list[i] != nullptr; ++i)
+	for (int i{}; ptr->h_addr_list[i] != nullptr; ++i)
 	{
 		memcpy(&addr, ptr->h_addr_list[i], ptr->h_length);
 		inet_ntop(AF_INET, &addr, str, sizeof(str));
@@ -30,24 +43,4 @@ void domainCheck(const char* name)
 	}
 
 	std::cout << std::endl;
-}
-
-int main(int argc, char* argv[])
-{
-	// 윈속 초기화
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return 1;
-
-	std::string str;
-
-	while (true)
-	{
-		std::cout << "도메인을 입력해주세요: ";
-		std::cin >> str;
-
-		// 도메인 이름 -> IP 주소
-		struct in_addr addr;
-		domainCheck(str.c_str());
-	}
 }
